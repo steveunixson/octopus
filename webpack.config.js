546f5315c03d12342e6a76598a961de0cb7dcd5d
Steve Unixson <unixson@gmail.com>
2019-06-01 00:12:00 +0300

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: './octopus.lib.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+};
