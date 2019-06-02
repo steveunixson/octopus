1cc007b714bd11a9994565ac6365982dd88464e4
Steve Unixson <unixson@gmail.com>
2019-06-02 23:55:56 +0300

    just a start

---
+++ src/classes/main.class.ts
@@ -0,0 +1,9 @@
+// here we bootstrap our browsers or native apps (for opencv bug searching subsystem)
+// like Firefox or Chromium (puppeteer) or M$ Outlook (for some shitty reason)
+// create class instance of an environment you are using
+// supported browsers for now will be:
+// Firefox and Chrome
+// IE and EDGE will be opened on a dedicated vm, because well... fuck you that's why
+// and that's all folks! No Safari for now, because I don't have a mac
+// Maybe I should get one
+// Just kidding I am a broke ass
