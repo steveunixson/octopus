ffdb0ba0e6a0a71d3d100ef832ac774f10d1828d
steveunixson <unixson@gmail.com>
2019-06-06 17:34:49 +0300

    clean up a little bit

--- src/classes/main.class.ts
+++ src/classes/main.class.ts
@@ -1,12 +1,14 @@
-// here we bootstrap our browsers or native apps (for opencv bug searching subsystem)
-// like Firefox or Chromium (puppeteer) or M$ Outlook (for some shitty reason)
-// create class instance of an environment you are using
-// supported browsers for now will be:
-// Firefox and Chrome
-// IE and EDGE will be opened on a dedicated vm, because well... fuck you that's why
-// and that's all folks! No Safari for now, because I don't have a mac
-// Maybe I should get one
-// Just kidding I am a broke ass
+/*
+here we bootstrap our browsers or native apps (for opencv bug searching subsystem)
+like Firefox or Chromium (puppeteer) or M$ Outlook (for some shitty reason)
+create class instance of an environment you are using
+supported browsers for now will be:
+Firefox and Chrome
+IE and EDGE will be opened on a dedicated vm
+and that's all folks! No Safari for now, because I don't have a mac
+Maybe I should get one
+Just kidding I am a broke ass
+*/
 import * as puppeteer from 'puppeteer';
 import {Browser, Page} from "puppeteer";
 
@@ -18,10 +20,10 @@ interface Settings {
 }
 
 export default class Bootstrap {
-    public width: number;
-    public height: number;
-    public timeout: number;
-    public headless: boolean;
+    private readonly width: number;
+    private readonly height: number;
+    private readonly timeout: number;
+    private readonly headless: boolean;
     public browser: Browser;
     public page: Page;
     constructor(settings: Settings) {
