90088817ecd6d64c43c6e8fca6a9f73f274e0b7f
steveunixson <unixson@gmail.com>
2019-06-21 16:54:38 +0300

    added needed exports for so it can be used as a library

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,19 @@ import Bootstrap from "./classes/main.class";
 import HelpersClass, {log, VisionHelper} from "./helpers/helpers.class";
 import VisionClass from "./classes/vision.class";
 
-export default class Octopus {
-    private instance: Bootstrap;
-    private vision: VisionClass;
+export interface OctopusInterface {
+    instance: Bootstrap;
+    vision: VisionClass;
+    createBrowser(url: string): Promise<void>;
+}
+
+export { HelpersClass as HelpersClass };
+export { VisionClass as  VisionClass };
+export { VisionHelper as VisionHelper }
+
+export default class Octopus implements OctopusInterface {
+    public instance: Bootstrap;
+    public vision: VisionClass;
     constructor() {
         this.instance = new Bootstrap({
             width: 1440,
@@ -19,32 +29,4 @@ export default class Octopus {
        await HelpersClass.tryURL(url);
        await this.instance.page.goto(url);
     }
-
-    public async testGmail() {
-       await this.createBrowser('https://google.com');
-       await this.vision.waitFor('/home/unixson/WebstormProjects/octopus/images/login.png', 10);
-       await this.vision.findAndMoveTo('/home/unixson/WebstormProjects/octopus/images/login.png');
-       await VisionHelper.click();
-       await this.vision.waitFor('/home/unixson/WebstormProjects/octopus/images/loginBox.png', 10);
-       await VisionHelper.type('unixson');
-       await VisionHelper.key('shift', 'down');
-       await VisionHelper.type('2');
-       await VisionHelper.key('shift', 'up');
-       await VisionHelper.type('gmail.com');
-       await this.vision.findAndMoveTo('/home/unixson/WebstormProjects/octopus/images/next.png');
-       await VisionHelper.click();
-       await this.vision.waitFor('/home/unixson/WebstormProjects/octopus/images/loginBoxNext.png', 10);
-       await VisionHelper.type('loastcoast1997');
-       await this.vision.findAndMoveTo('/home/unixson/WebstormProjects/octopus/images/next.png');
-       await VisionHelper.click();
-       await this.vision.waitFor('/home/unixson/WebstormProjects/octopus/images/loginSuccess.png', 10);
-       await this.instance.browser.close();
-    }
 }
-
-const octopus = new Octopus();
-    octopus.testGmail()
-        .then(() => {
-            log.info('TEST FINISHED')
-        })
-        .catch((e) => { console.log(e.toString()) });
